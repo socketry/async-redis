98a556b9c3d48dfe9e51e5310e3c0f09b55a0617
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2018-12-12 01:14:19 +1300

    Prefer require_relative.

--- lib/async/redis.rb
+++ lib/async/redis.rb
@@ -18,4 +18,5 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-require "async/redis/version"
+require_relative 'redis/version'
+require_relative 'redis/client'
