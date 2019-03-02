fa1244a30e436d53706b941570e84eecad1ebd1c
k1tsu <45673607+k1tsu@users.noreply.github.com>
2019-03-03 08:32:00 +1300

    Added performance spec for async/redis vs redis-rb/redis (Fixes #8).

--- async-redis.gemspec
+++ async-redis.gemspec
@@ -21,7 +21,9 @@ Gem::Specification.new do |spec|
 	spec.add_dependency("async-io", "~> 1.10")
 	
 	spec.add_development_dependency "async-rspec", "~> 1.1"
-	
+	spec.add_development_dependency "redis"
+	spec.add_development_dependency "benchmark-ips"
+
 	spec.add_development_dependency "covered"
 	spec.add_development_dependency "bundler"
 	spec.add_development_dependency "rspec", "~> 3.6"
---
+++ spec/async/redis/redis_peformance_spec.rb
@@ -0,0 +1,54 @@
+# Copyright, 2019, by Pierre Montelle.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require 'async/redis'
+require 'redis'
+require 'benchmark/ips'
+
+RSpec.describe "Client Performance" do
+	it "should be fast to set keys" do
+		Benchmark.ips do |x|
+			x.report("async-redis") do |times|
+				endpoint = Async::Redis.local_endpoint
+
+				Async do
+					client = Async::Redis::Client.new(endpoint)
+
+					while (times -= 1) >= 0
+						client.set(["X","Y","Z"].sample, rand(1..10))
+					end
+
+				# ensure
+					client.close
+				end
+			end
+
+			x.report("redis-rb") do |times|
+				redis = Redis.new
+
+				while (times -= 1) >= 0
+					redis.set(["X","Y","Z"].sample, rand(1..10))
+				end
+			end
+			x.compare!
+
+		end
+	end
+end
