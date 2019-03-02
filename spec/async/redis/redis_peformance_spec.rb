42ab9853a8b26246aa64d2cb873d763fe145bcdb
Pierre Montelle <k1tsu@Pierres-Air.fritz.box>
2019-03-03 02:35:27 +1300

    Fixed typo

--- spec/async/redis/redis_peformance_spec.rb
+++ spec/async/redis/redis_peformance_spec.rb
@@ -18,7 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-require "rspec"
+require "RSpec"
 require 'async/redis'
 require 'redis'
 require 'benchmark/ips'
