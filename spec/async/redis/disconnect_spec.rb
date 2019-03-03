a68c1e68e2477f0c816eab8957fe4e71eb0731d5
Jeremy Jung <jeremyjung@users.noreply.github.com>
2019-03-03 11:46:37 -0800

    Add spec check for EOFError on disconnect (#11)

---
+++ spec/async/redis/disconnect_spec.rb
@@ -0,0 +1,26 @@
+require 'async/redis/client'
+
+RSpec.describe Async::Redis::Client, timeout: 5 do
+	include_context Async::RSpec::Reactor
+
+	let(:endpoint) { Async::IO::Endpoint.tcp('localhost', 5555) }
+
+	it "should raise EOFError on unexpected disconnect" do
+		server_task = reactor.async do
+			endpoint.accept do |connection|
+				stream = Async::IO::Stream.new(connection)
+				stream.read(8)
+				stream.close
+				connection.close
+			end
+		end
+
+		reactor.async do
+			client = Async::Redis::Client.new(endpoint)
+			expect { client.call("GET", "test") }.to raise_error(EOFError)
+			client.close
+			server_task.stop
+		end
+
+	end
+end
