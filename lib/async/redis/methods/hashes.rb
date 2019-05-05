ee79fc589b7c7adaef157a5c23f4c78a1c10fb7e
Mika Hel <mikael@zoolutions.se>
2019-05-06 00:30:54 +0200

    Correct copyright

--- lib/async/redis/methods/hashes.rb
+++ lib/async/redis/methods/hashes.rb
@@ -1,5 +1,4 @@
-# Copyright, 2018, by Samuel G. D. Williams. <http://www.codeotaku.com>
-# Copyright, 2018, by Huba Nagy.
+# Copyright, 2018, by Mikael Henriksson. <http://www.mhenrixon.com>
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
