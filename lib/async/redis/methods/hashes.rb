3ebcb2e9df9ab8db1ecd0f747255a74b98e17209
Mika Hel <mikael@zoolutions.se>
2019-05-06 00:42:47 +0200

    Increment the copyright year

--- lib/async/redis/methods/hashes.rb
+++ lib/async/redis/methods/hashes.rb
@@ -1,4 +1,4 @@
-# Copyright, 2018, by Mikael Henriksson. <http://www.mhenrixon.com>
+# Copyright, 2019, by Mikael Henriksson. <http://www.mhenrixon.com>
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
