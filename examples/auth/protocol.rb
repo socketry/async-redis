61615928450d602d7bbcab10fe1780f74111200f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-05-11 14:15:38 +1200

    Auth example.

---
+++ examples/auth/protocol.rb
@@ -0,0 +1,26 @@
+
+require_relative '../../lib/async/redis'
+
+class AuthenticatedRESP2
+	def initialize(credentials, protocol: Async::Redis::Protocol::RESP2)
+		@credentials = credentials
+		@protocol = protocol
+	end
+
+	def client(stream)
+		client = @protocol.client(stream)
+		
+		client.write_request(["AUTH", *@credentials])
+		client.read_response # Ignore response.
+		
+		return client
+	end
+end
+
+Async do
+	endpoint = Async::Redis.local_endpoint
+	
+	client = Async::Redis::Client.new(endpoint, protocol: AuthenticatedRESP2.new(["username", "password"]))
+	
+	pp client.info
+end
