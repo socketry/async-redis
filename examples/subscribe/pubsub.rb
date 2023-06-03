36f378695a76f345adf64e2da3533c6ab9506994
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-06-04 00:41:46 +0900

    Add pubsub example.

---
+++ examples/subscribe/pubsub.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+require_relative '../../lib/async/redis'
+
+class Subscription
+	def initialize(topic, endpoint = Async::Redis.local_endpoint)
+		@topic = topic
+		@endpoint = endpoint
+		@client = nil
+	end
+	
+	def client
+		@client ||= Async::Redis::Client.new(@endpoint)
+	end
+
+	def subscribe
+		client.subscribe(@topic) do |context|
+			while event = context.listen
+				yield event
+			end
+		end
+	end
+
+	def publish(message)
+		client.publish @topic, message
+	end
+end
+
+Sync do |task|
+	subscription = Subscription.new("my-topic")
+	
+	subscriber = task.async do
+		subscription.subscribe do |message|
+			pp message
+		end
+	end
+	
+	10.times do |i|
+		subscription.publish("Hello World #{i}")
+	end
+	
+	subscriber.stop
+end
