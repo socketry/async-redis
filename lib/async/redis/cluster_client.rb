2263c69a54131f3502770198368a23eebf2f42d4
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-08-14 00:26:04 +1200

    Better orgnization.

--- lib/async/redis/cluster_client.rb
+++ lib/async/redis/cluster_client.rb
@@ -5,7 +5,8 @@
 # Copyright, 2025, by Travis Bell.
 
 require_relative "client"
-require_relative "context/shard_subscribe"
+require_relative "cluster_subscribe"
+require_relative "range_map"
 require "io/stream"
 
 module Async
@@ -21,64 +22,6 @@ module Async
 			end
 			
 			Node = Struct.new(:id, :endpoint, :role, :health, :client)
-			
-			# A map that stores ranges and their associated values for efficient lookup.
-			class RangeMap
-				# Initialize a new RangeMap.
-				def initialize
-					@ranges = []
-				end
-				
-				# Add a range-value pair to the map.
-				# @parameter range [Range] The range to map.
-				# @parameter value [Object] The value to associate with the range.
-				# @returns [Object] The added value.
-				def add(range, value)
-					@ranges << [range, value]
-					
-					return value
-				end
-				
-				# Find the value associated with a key within any range.
-				# @parameter key [Object] The key to find.
-				# @yields {...} Block called if no range contains the key.
-				# @returns [Object] The value if found, result of block if given, or nil.
-				def find(key)
-					@ranges.each do |range, value|
-						return value if range.include?(key)
-					end
-					
-					if block_given?
-						return yield
-					end
-					
-					return nil
-				end
-				
-				# Iterate over all values in the map.
-				# @yields {|value| ...} Block called for each value.
-				# 	@parameter value [Object] The value from the range-value pair.
-				def each
-					@ranges.each do |range, value|
-						yield value
-					end
-				end
-				
-				# Get a random value from the map.
-				# @returns [Object] A randomly selected value, or nil if map is empty.
-				def sample
-					return nil if @ranges.empty?
-					
-					range, value = @ranges.sample
-					
-					return value
-				end
-				
-				# Clear all ranges from the map.
-				def clear
-					@ranges.clear
-				end
-			end
 			
 			# Create a new instance of the cluster client.
 			#
@@ -166,7 +109,7 @@ module Async
 				@endpoints.each do |endpoint|
 					client = Client.new(endpoint, **@options)
 					
-					shards = RangeMap.new
+					shards = Async::Redis::RangeMap.new
 					endpoints = []
 					
 					client.call("CLUSTER", "SHARDS").each do |shard|
@@ -337,9 +280,12 @@ module Async
 			# @returns [Object] The result of the block if block given.
 			# @returns [Context::ShardSubscribe] The shard subscription context if no block given.
 			def ssubscribe(*channels)
-				context = Context::ShardSubscribe.new(self)
-				context.subscribe(channels) if channels.any?
+				context = ClusterSubscribe.new(self)
 				
+				if channels.any?
+					context.subscribe(channels)
+				end
+
 				if block_given?
 					begin
 						yield context
---
+++ lib/async/redis/range_map.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module Async
+	module Redis
+		# A map that stores ranges and their associated values for efficient lookup.
+		class RangeMap
+			# Initialize a new RangeMap.
+			def initialize
+				@ranges = []
+			end
+
+			# Add a range-value pair to the map.
+			# @parameter range [Range] The range to map.
+			# @parameter value [Object] The value to associate with the range.
+			# @returns [Object] The added value.
+			def add(range, value)
+				@ranges << [range, value]
+				return value
+			end
+
+			# Find the value associated with a key within any range.
+			# @parameter key [Object] The key to find.
+			# @yields {...} Block called if no range contains the key.
+			# @returns [Object] The value if found, result of block if given, or nil.
+			def find(key)
+				@ranges.each do |range, value|
+					return value if range.include?(key)
+				end
+				if block_given?
+					return yield
+				end
+				return nil
+			end
+
+			# Iterate over all values in the map.
+			# @yields {|value| ...} Block called for each value.
+			#  @parameter value [Object] The value from the range-value pair.
+			def each
+				@ranges.each do |range, value|
+					yield value
+				end
+			end
+
+			# Get a random value from the map.
+			# @returns [Object] A randomly selected value, or nil if map is empty.
+			def sample
+				return nil if @ranges.empty?
+				range, value = @ranges.sample
+				return value
+			end
+
+			# Clear all ranges from the map.
+			def clear
+				@ranges.clear
+			end
+		end
+	end
+end
