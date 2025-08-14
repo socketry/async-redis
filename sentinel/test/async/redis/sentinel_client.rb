0fe62340775667256395100c5b1a64ee1b0ba226
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-08-14 21:12:52 +1200

    Tidy up tests.

--- sentinel/test/async/redis/sentinel_client.rb
+++ sentinel/test/async/redis/sentinel_client.rb
@@ -33,15 +33,18 @@ describe Async::Redis::SentinelClient do
 		expect(client.get(key)).to be == value
 	end
 	
+	def wait_until(duration: 0.001, repeats: 100)
+		repeats.times do
+			break if yield
+			sleep duration
+		end
+	end
+	
 	it "should resolve slave address" do
 		client.set(key, value)
 		
-		# It takes a while to replicate:
-		timeout_count = 0
-		while timeout_count < 100 # Maximum 1 second wait
-			break if slave_client.get(key) == value
-			sleep 0.01
-			timeout_count += 1
+		wait_until do
+			slave_client.get(key) == value
 		end
 		
 		expect(slave_client.get(key)).to be == value
@@ -60,7 +63,7 @@ describe Async::Redis::SentinelClient do
 			master_options = {database: 1}
 			client_with_options = subject.new(sentinels, master_options: master_options, role: :master)
 			
-			# Verify the client can set/get values (basic connectivity)
+			# Verify the client can set/get values (basic connectivity):
 			client_with_options.set(key, value)
 			expect(client_with_options.get(key)).to be == value
 		end
@@ -69,20 +72,13 @@ describe Async::Redis::SentinelClient do
 			slave_options = {database: 2}
 			slave_client_with_options = subject.new(sentinels, slave_options: slave_options, role: :slave)
 			
-			# Set data via master that also uses the same database
+			# Set data via master that also uses the same database:
 			master_client_with_options = subject.new(sentinels, master_options: slave_options, role: :master)
 			master_client_with_options.set(key, value)
 			
-			# Wait for replication and verify slave can read (basic connectivity)
-			timeout_count = 0
-			while timeout_count < 100 # Maximum 1 second wait
-				begin
-					break if slave_client_with_options.get(key) == value
-				rescue
-					# May fail initially due to replication lag or connection setup
-				end
-				sleep 0.01
-				timeout_count += 1
+			# Wait for replication and verify slave can read (basic connectivity):
+			wait_until do
+				slave_client_with_options.get(key) == value
 			end
 			
 			expect(slave_client_with_options.get(key)).to be == value
@@ -92,20 +88,13 @@ describe Async::Redis::SentinelClient do
 			master_options = {database: 3}
 			slave_client_fallback = subject.new(sentinels, master_options: master_options, role: :slave)
 			
-			# Set data via master first  
+			# Set data via master first:
 			client_with_master_options = subject.new(sentinels, master_options: master_options, role: :master)
 			client_with_master_options.set(key, value)
 			
-			# Wait for replication and verify slave can read using master options
-			timeout_count = 0
-			while timeout_count < 100 # Maximum 1 second wait
-				begin
-					break if slave_client_fallback.get(key) == value
-				rescue
-					# May fail initially due to replication lag or connection setup
-				end
-				sleep 0.01
-				timeout_count += 1
+			# Wait for replication and verify slave can read using master options:
+			wait_until do
+				slave_client_fallback.get(key) == value
 			end
 			
 			expect(slave_client_fallback.get(key)).to be == value
