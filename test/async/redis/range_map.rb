abeb55f5493ab06db863a2fa812b083a6c66e26e
Samuel Williams <samuel.williams@shopify.com>
2026-08-25 17:03:24 +1200

    Improve `RangeMap` unit test coverage. (#75)

--- test/async/redis/range_map.rb
+++ test/async/redis/range_map.rb
@@ -8,22 +8,55 @@ require "async/redis/range_map"
 describe Async::Redis::RangeMap do
 	let(:map) {subject.new}
 	
-	it "can associate multiple ranges with one value" do
-		map.add([0..5, 10..15], :first)
-		map.add(6..9, :second)
+	with "#add and #find" do
+		it "can associate a single range with a value" do
+			expect(map.add(0..5, :value)).to be == :value
+			expect(map.find(3)).to be == :value
+		end
 		
-		expect(map.find(0)).to be == :first
-		expect(map.find(12)).to be == :first
-		expect(map.find(7)).to be == :second
+		it "can associate multiple ranges with one value" do
+			map.add([0..5, 10..15], :value)
+			
+			expect(map.find(0)).to be == :value
+			expect(map.find(12)).to be == :value
+			expect(map.find(7)).to be_nil
+		end
+		
+		it "can provide a fallback for missing keys" do
+			expect(map.find(5)).to be_nil
+			expect(map.find(5){:missing}).to be == :missing
+		end
 	end
 	
-	it "treats grouped ranges as one mapping" do
-		map.add([0..5, 10..15], :first)
-		
-		values = []
-		map.each{|value| values << value}
-		
-		expect(values).to be == [:first]
-		expect(map.sample).to be == :first
+	with "#each" do
+		it "yields once for each mapping" do
+			map.add([0..5, 10..15], :first)
+			map.add(6..9, :second)
+			
+			values = []
+			map.each{|value| values << value}
+			
+			expect(values).to be == [:first, :second]
+		end
+	end
+	
+	with "#sample" do
+		it "returns a mapped value" do
+			expect(map.sample).to be_nil
+			
+			map.add([0..5, 10..15], :value)
+			
+			expect(map.sample).to be == :value
+		end
+	end
+	
+	with "#clear" do
+		it "removes all mappings" do
+			map.add(0..5, :value)
+			map.clear
+			
+			expect(map.find(3)).to be_nil
+			expect(map.sample).to be_nil
+		end
 	end
 end
